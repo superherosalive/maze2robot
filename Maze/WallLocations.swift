27029c480f628192f709d05bb2aa413428dee688
Eric Nguyen <ericnguyen@erics-imac.local>
2016-02-17 00:53:42 -0600

    Initial Commit

---
+++ Maze/WallLocations.swift
@@ -0,0 +1,49 @@
+//
+//  WallLocations.swift
+//  Maze
+//
+//  Created by Gabrielle Miller-Messner on 11/6/15.
+//  Copyright © 2015 Udacity, Inc. All rights reserved.
+//
+
+import Foundation
+
+extension ControlCenter {
+    
+    func isFacingWall(robot: ComplexRobotObject, direction: MazeDirection) -> Bool {
+        
+        let cell = mazeController.currentCell(robot)
+        var isWall: Bool = false
+        
+        // Step 1.1a
+        // TODO: Write a switch statement handling all possible values of direction. The first case has been done for you. Uncomment the code and add the remaining cases!
+                switch(direction) {
+                case .Up:
+                    if cell.top {
+                        isWall = true
+                    }
+                case .Down:
+                    if cell.bottom {
+                        isWall = true
+                    }
+                case .Left:
+                    if cell.left {
+                        isWall = true
+                    }
+                case .Right:
+                    if cell.right {
+                        isWall = true
+                    }
+                    
+                                   
+
+                }
+        
+        // Step 1.1b
+        // TODO: Return a Bool that represents whether the robot is currently facing a wall. You will need to change the placeholder return statement below.
+        
+        
+        // Placeholder
+        return isWall
+    }
+}
